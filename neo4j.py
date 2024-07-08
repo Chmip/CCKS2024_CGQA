390842be2b3deb3214574e41814483eef9663dcd
Chmip <108646877+Chmip@users.noreply.github.com>
2024-07-08 19:24:04 +0800

    Add files via upload

--- neo4j.py
+++ neo4j.py
@@ -1,25 +1,33 @@
 from py2neo import Graph, Node, Relationship, NodeMatcher, RelationshipMatcher
 import os
-r_t = {'老师': ['学生'], '作者': ['代表作品', '文学作品'], '祖父': ['孙子'], '儿子': ['父亲', '母亲'], '女儿': ['父亲', '母亲'], '丈夫': ['妻子'], '曾孙子': ['曾祖父']}
-r_temp = {}
-for r_index, r_list in r_t.items():
-    for relation in r_list:
-        if relation not in r_temp:
-            r_temp[relation] = []
-        r_temp[relation].append(r_index)
-#print(r_temp)
-#r_t.update(r_temp)
-#print(r_t)
-r_t = {'老师': ['学生'], '作者': ['代表作品', '文学作品'], '祖父': ['孙子'], '儿子': ['父亲', '母亲'], '女儿': ['父亲', '母亲'], '丈夫': ['妻子'], '曾孙子': ['曾祖父'], '学生': ['老师'], '代表作品': ['作者'], '文学作品': ['作者'], '孙子': ['祖父'], '妻子': ['丈夫'], '曾祖父': ['曾孙子'], '音乐作品': ['歌曲原唱'], '歌曲原唱': ['音乐作品']}
-r_y = {'大舅': ['大舅子'], '爷爷': ['大爷爷'], '现任妻子': ['妻子'], '现任丈夫': ['丈夫'], '子女': ['儿子', '女儿'], '演员': ['主要演员'], '主演': ['主要演员'], '家庭成员': ['儿子','女儿', '妻子', '丈夫'], '祖先': ['祖父', '曾祖父'], '孩子': ['儿子', '女儿'], '直系亲属': ['父亲', '外祖父', '妻子'], '堂兄弟': ['堂兄', '堂弟'], '原唱': ['歌曲原唱'], '出版': ['出版社'], '作品': ['文学作品'], '下属机构': ['所属机构'], '出演': ['参演']}
-#r_t = {'老师': '学生', '作者': ['代表作品', '文学作品'], '祖父': '孙子', '儿子': ['父亲', '母亲'], '丈夫': '妻子', '孙子': '曾祖父'}
 
 
-r_mapping = {'儿子': [['妻子', '儿子']]}
-r_mapper = {'儿子': [['父亲', '孙子']], '爷爷': [['父亲', '父亲']]}
 class AnswerSearcher:
     def __init__(self):
         self.g = Graph("http://localhost:7474", auth=("neo4j", "123456"), name="neo4j")
+        self.r_replace = {'大舅': ['大舅子'], '爷爷': ['祖父'], '现任妻子': ['妻子'], '现任丈夫': ['丈夫'],
+                          '子女': ['儿子', '女儿'], '演员': ['主要演员'], '主演': ['主要演员'],
+                          '家庭成员': ['儿子', '女儿', '妻子', '丈夫'], '祖先': ['祖父', '曾祖父'],
+                          '孩子': ['儿子', '女儿'], '直系亲属': ['父亲', '外祖父', '妻子'], '堂兄弟': ['堂兄', '堂弟'],
+                          '原唱': ['歌曲原唱'], '出版': ['出版社'], '作品': ['文学作品'], '下属机构': ['所属机构'],
+                          '出演': ['参演'], '专辑': ['发行专辑'], '毕业': ['毕业院校'], '合作': ['搭档'],
+                          '书': ['文学作品', '代表作品'], '为他人创作的音乐': ['为他人创作音乐'],
+                          '创作': ['为他人创作音乐']}
+
+        self.r_mapper = {'儿子': [['父亲', '孙子'], ['妻子', '儿子']], '祖父': [['父亲', '父亲']], '父亲': [['祖父', '儿子']], '曾祖父': [['祖父', '父亲']],}
+
+        self.r_transform = {'老师': ['学生'], '作者': ['代表作品', '文学作品'], '祖父': ['孙子'], '爷爷': ['孙子'],'儿子': ['父亲', '母亲'], '女儿': ['父亲', '母亲'], '丈夫': ['妻子'], '曾孙子': ['曾祖父'], '音乐作品': ['歌曲原唱'],}
+        self.initialize()
+
+    def initialize(self):
+        r_temp = {}
+        for r_index, r_list in self.r_transform.items():
+            for relation in r_list:
+                if relation not in r_temp:
+                    r_temp[relation] = []
+                r_temp[relation].append(r_index)
+        self.r_transform.update(r_temp)
+
 
     def default(self, E1, E2):
         if E1 == None:
@@ -33,9 +41,9 @@ class AnswerSearcher:
         return E1, E2
 
     def default_R(self, R):
-        if R in r_y:
-            if len(r_y[R]) == 1:
-                return r_y[R][0]
+        if R in self.r_replace:
+            if len(self.r_replace[R]) == 1:
+                return self.r_replace[R][0]
         return R
 
     def e(self, E, Ename):
@@ -50,49 +58,50 @@ class AnswerSearcher:
         for result in results:
             print(str(result['p'].labels)[1:])
 
+    def er_(self, E1name, R, E1=None, E2=None):
+        last_result = []
+        if R in self.r_replace:
+            for item in self.r_replace[R]:
+                result = self.er_mapper(E1name, item)
+                for result_item in result:
+                    last_result.append(result_item)
+        else:
+            last_result = self.er_mapper(E1name, R)
+        return last_result
+
 
     def er_mapper(self,E1name, R, E1=None, E2=None):
-        last_result = set()
-        if R in r_mapping:
-            for mapper_list in r_mapping[R]:
+        last_result = set(self.er_transform(E1name, R))
+        if R in self.r_mapper:
+            for mapper_list in self.r_mapper[R]:
                 result = [E1name]
                 for mapper_item in mapper_list:
                     if len(result) > 0:
-                        result = self.er_0(E1, result[0], mapper_item, E2)
+                        print(result[0], mapper_item)
+                        result = self.er_transform(result[0], mapper_item)
+
                     else:
                         break
                 if len(result) > 0:
                     last_result.update(result)
-        last_result.update(self.er_0(E1, E1name, R, E2))
         return list(last_result)
 
-    def er_(self, E1name, R, E1=None, E2=None):
-        last_result = set(self.er_0(E1, E1name, R, E2))
-        if len(last_result) == 0:
-            if R in r_mapping:
-                for mapper_list in r_mapping[R]:
-                    result = [E1name]
-                    for mapper_item in mapper_list:
-                        if len(result) > 0:
-                            result = self.er_0(E1, result[0], mapper_item, E2)
-                        else:
-                            break
-                    if len(result) > 0:
-                        last_result.update(result)
+
+
+    def er_transform(self, E1name, R, E1=None, E2=None):
+        R = self.default_R(R)
+        last_result = set(self.er_0(E1name, R))
+        if R in self.r_transform:
+            for item in self.r_transform[R]:
+                result = self._re(item, E1name)
+                last_result.update(result)
         return list(last_result)
 
     def er_0(self, E1name, R, E1=None, E2=None):
         last_result = []
-        R = self.default_R(R)
-        if R in r_t:
-            for item in r_t[R]:
-                result = self._re(item, E1name)
-                for result_item in result:
-                    last_result.append(result_item)
-
         E1, E2 = self.default(E1, E2)
+        R = self.default_R(R)
         query = f'MATCH (p{E1})-[:`{R}`]->(m{E2}) where p.name="{E1name}" return m.name'
-        #print(query)
         results = self.g.run(query)
         for result in results.data():
             last_result.append(result['m.name'])
@@ -102,6 +111,7 @@ class AnswerSearcher:
 
     def _re(self, R, E2name, E1=None, E2=None):
         E1, E2 = self.default(E1, E2)
+        R = self.default_R(R)
         query = f'MATCH (p{E1})-[:`{R}`]->(m{E2}) where m.name="{E2name}" return p.name'
         #print(query)
         results = self.g.run(query)
@@ -132,45 +142,83 @@ class AnswerSearcher:
 
     def get_result(self, ref, mapper):
         if ref == '+':
-            return self.er_0(mapper[0], mapper[1])
+            return self.er_transform(mapper[0], mapper[1])
         elif ref == '-':
             return self._re(mapper[0], mapper[1])
 
         elif ref == '+c':
-            result = len(self.er_0(mapper[0], mapper[1]))
+            result = len(self.er_transform(mapper[0], mapper[1]))
             return [str(result)]
 
-        elif ref == '++':
-            result = self.er_0(mapper[0], mapper[1])
+        elif ref == '+c+c':
+            result1 = len(self.er_transform(mapper[0], mapper[1]))
+            result2 = len(self.er_transform(mapper[2], mapper[3]))
+            return [str(result1 + result2)]
+
+        elif ref == '-c':
+            result = len(self._re(mapper[0], mapper[1]))
+            return [str(result)]
+
+        elif ref == '+>+':
+            result = self.er_transform(mapper[0], mapper[1])
             if len(result) == 0:
                 return ["知识库未提及"]
-            return self.er_0(result[0], mapper[2])
+            return self.er_transform(result[0], mapper[2])
 
-        elif ref == '+++':
-            result1 = set(self.er_0(mapper[0], mapper[1]))
-            result2 = set(self.er_0(mapper[2], mapper[3]))
+        elif ref == '++&':
+            result1 = set(self.er_transform(mapper[0], mapper[1]))
+            result2 = set(self.er_transform(mapper[2], mapper[3]))
             return list(result1 & result2)
 
-        elif ref == '+++c':
-            result1 = set(self.er_0(mapper[0], mapper[1]))
-            result2 = set(self.er_0(mapper[2], mapper[3]))
+        elif ref == '+l+':
+            result1 = set(self.er_transform(mapper[0], mapper[1]))
+            result2 = set(self.er_transform(mapper[2], mapper[3]))
+            return list(result1) + list(result2)
+
+        elif ref == '++&c':
+            result1 = set(self.er_transform(mapper[0], mapper[1]))
+            result2 = set(self.er_transform(mapper[2], mapper[3]))
             result = len(result1 & result2)
             if result == 0:
                 return ["无"]
             return [str(result)]
 
-        elif ref == '+++j':
-            result1 = set(self.er_0(mapper[0], mapper[1]))
-            result2 = set(self.er_0(mapper[2], mapper[3]))
+        elif ref == '++|c':
+            result1 = set(self.er_transform(mapper[0], mapper[1]))
+            result2 = set(self.er_transform(mapper[2], mapper[3]))
+            result = len(result1 | result2)
+            if result == 0:
+                return ["无"]
+            return [str(result)]
+
+        elif ref == '+-&c':
+            result1 = set(self.er_transform(mapper[0], mapper[1]))
+            result2 = set(self._re(mapper[2], mapper[3]))
             result = len(result1 & result2)
             if result == 0:
-                return ["没有"]
+                return ["无"]
+            return [str(result)]
+
+        elif ref == '+-|c':
+            result1 = set(self.er_transform(mapper[0], mapper[1]))
+            result2 = set(self._re(mapper[2], mapper[3]))
+            result = len(result1 | result2)
+            if result == 0:
+                return ["无"]
+            return [str(result)]
+
+        elif ref == '++&j':
+            result1 = set(self.er_transform(mapper[0], mapper[1]))
+            result2 = set(self.er_transform(mapper[2], mapper[3]))
+            result = len(result1 & result2)
+            if result == 0:
+                return ["无"]
             return ['有']
 
 
 
-        elif ref == '+-':
-            result1 = set(self.er_0(mapper[0], mapper[1]))
+        elif ref == '+-&':
+            result1 = set(self.er_transform(mapper[0], mapper[1]))
             result2 = set(self._re(mapper[2], mapper[3]))
             return list(result1 & result2)
 
@@ -178,8 +226,7 @@ class AnswerSearcher:
 
 
 
-
-        elif ref == '--':
+        elif ref == '--&':
             result1 = set(self._re(mapper[0], mapper[1]))
             result2 = set(self._re(mapper[2], mapper[3]))
             return list(result1 & result2)
@@ -187,10 +234,10 @@ class AnswerSearcher:
 
 
         elif ref == '+l+-':
-            result1 = self.er_0(mapper[0], mapper[1])
+            result1 = self.er_transform(mapper[0], mapper[1])
             if len(result1) == 0:
                 return ["知识库未提及"]
-            result2 = self.er_0(result1[0], mapper[2])
+            result2 = self.er_transform(result1[0], mapper[2])
             if mapper[0] in result2:
                 result2.remove(mapper[0])
             return [result1[0]] + result2
@@ -208,6 +255,13 @@ class AnswerSearcher:
                 return list(result)
             return ["知识库未提及"]
 
+        elif ref == '<>j':
+            result = set(self.e_e(mapper[0], mapper[1]))
+            if len(result) == 0:
+                return ["否"]
+            elif len(result) > 0:
+                return ["是"]
+
 
         elif ref == '<><>':
             result1 = self.get_result('<>', mapper[0:2])
@@ -216,20 +270,25 @@ class AnswerSearcher:
 
 
 
+        else:
+            print('不存在：', ref)
+
+
+
 
 def test():
     searcher = AnswerSearcher()
-    '''print(searcher.er_0("纽约纽约", "主要演员"))#		欧阳靖
+    '''print(searcher.er_transform("纽约纽约", "主要演员"))#		欧阳靖
     mapper = ['朴叙俊', '搭档', '寄生虫', '主要演员']
-    result1 = set(searcher.er_0(mapper[0], mapper[1]))
-    result2 = set(searcher.er_0(mapper[2], mapper[3]))
+    result1 = set(searcher.er_transform(mapper[0], mapper[1]))
+    result2 = set(searcher.er_transform(mapper[2], mapper[3]))
     print(result1)
     print(result2)
 
     print(result1 & result2)
 
     mapper = ['狼殿下', '主要演员']
-    result1 = set(searcher.er_0(mapper[0], mapper[1]))
+    result1 = set(searcher.er_transform(mapper[0], mapper[1]))
     result2 = set(searcher._re('参演', mapper[0]))
     print(len(result1))
     print(result2)
@@ -240,12 +299,28 @@ def test():
 
 
 
-    result1 = searcher.get_result('<><>', ['小池澈平', '玉置成实', '高明', '赋幽慵斋'])
+    '''result1 = searcher.get_result('+-&c', ['狼殿下', '主要演员', '参演', '狼殿下'])
+    print(result1)
+    result1 = searcher.get_result('+-|c', ['狼殿下', '主要演员', '参演', '狼殿下'])
     print(result1)
 
+    result1 = searcher.get_result('+c', ['白虹', '参演'])
+    print(result1)
+    result1 = searcher.get_result('+', ['灵笼', '主要配音'])
+    print(1,result1)
+    result1 = searcher.get_result('-', ['毕业','上海戏剧学院'])
+    print(2, result1)
+    result1 = searcher.get_result('++&c',['李建复', '音乐作品', '李建复', '代表作品'])
+    print(3,result1)
+    result1 = searcher.get_result('++&j', ['厄尔·巴隆', '毕业', '杰伦·杜伦', '毕业', '同一所大学'])
+    print(4,result1)'''
 
+
+
+    result = searcher.er_('七侠五义', '作者')
+    print(result)
 
 
     #searcher.ER_(None, "岭表纪年/所知录/天南逸史", "作者", None)
 
-#test()
+test()
